3e543aeb370f4aebd7a662de0d5490a9b0837ef1
Andreas Riemer <andreas.riemer@gmail.com>
2019-10-04 15:05:01 +0200

    Term implementiert die 2 neuen Funktionen

--- apps/jehovakel_ex_ecto/lib/ecto/term.ex
+++ apps/jehovakel_ex_ecto/lib/ecto/term.ex
@@ -7,4 +7,6 @@ defmodule Shared.Ecto.Term do
   def cast(term), do: {:ok, term}
   def load(bin), do: {:ok, bin |> :erlang.binary_to_term()}
   def dump(term), do: {:ok, term |> :erlang.term_to_binary()}
+  def embed_as(_), do: :self
+  def equal?(term1, term2), do: term1 == term2
 end
