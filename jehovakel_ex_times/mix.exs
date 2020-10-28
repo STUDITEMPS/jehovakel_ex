20178fc2a58b626485fb0497f1e6a7381ffd92bc
Andreas Riemer <andreas.riemer@gmail.com>
2020-10-28 19:55:15 +0100

    Bump Version to 1.0.0 for times

--- jehovakel_ex_times/mix.exs
+++ jehovakel_ex_times/mix.exs
@@ -4,15 +4,17 @@ defmodule JehovakelExTimes.MixProject do
   def project do
     [
       app: :jehovakel_ex_times,
-      version: "0.2.0",
-      build_path: "_build",
-      deps_path: "deps",
+      version: "1.0.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       test_paths: ["lib"],
       test_coverage: [tool: ExCoveralls],
-      deps: deps()
+      deps: deps(),
+      name: "Jehovakel EX Times",
+      source_url: "https://github.com/STUDITEMPS/jehovakel_ex/tree/master/jehovakel_ex_times",
+      description: description(),
+      package: package()
     ]
   end
 
@@ -33,4 +35,21 @@ defmodule JehovakelExTimes.MixProject do
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp description do
+    "TODO: describe this package"
+  end
+
+  defp package do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "jehovakel_ex_event_times",
+      # These are the default files included in the package
+      licenses: ["MIT License"],
+      links: %{
+        "GitHub" => "https://github.com/STUDITEMPS/jehovakel_ex/tree/master/jehovakel_ex_times",
+        "Studitemps" => "https://tech.studitemps.de"
+      }
+    ]
+  end
 end
